6dd002cb7abbdbe1124ffb4f9e4bddb8fc5505c6
VladimirBelyaev <vogisa@mail.ru>
2026-04-11 00:08:31 +0900

    feat: init app header

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AppHeader'
+})
+</script>
+
+<template>
+  <UHeader toggle-side="left">
+    <template #title>
+      SakhaGames
+    </template>
+  </UHeader>
+</template>
