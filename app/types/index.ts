0616ab4bbf1c721c45a1f622c2242b839477ceae
VladimirBelyaev <dazevvv@gmail.com>
2026-04-21 20:03:27 +0900

    feat: add base type helpers for PocketBase records

---
+++ app/types/index.ts
@@ -0,0 +1,14 @@
+export type RecordId = string
+
+export interface RecordTimestamps {
+  created: string
+  updated: string
+}
+
+export interface RecordDefault extends RecordTimestamps {
+  id: RecordId
+}
+
+export interface RecordSlug extends RecordDefault {
+  slug: string
+}
